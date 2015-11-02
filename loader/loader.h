bf42d9402f0257e9a0885228c0329bb0ce98df6a
dimok789 <dimok789@gmail.com>
2015-11-02 20:55:05 +0100

    Re-worked the complete loader code: Re-implemented the LiBounceOneChunk and LiWaitOneChunk functions and replaced the whole earlier loader functions by one LiWaitOneChunk function. (LiBounceOneChunk is just for...good to know ;-))
    Comment:
    This corrects the loading behavior. I don't think this will increase/decrease the compatibility (might be) as the loader behavior was almost correct before too but this reduces our loader code size by a lot, makes the loader code a lot more understandable and a lot easier to port to other system menu versions. Also since we don't need to fear GCC overwriting some registers anymore, the code optimizations can be used again which i re-enabled now too.

--- loader/loader.h
+++ loader/loader.h
@@ -1,16 +1,18 @@
 #ifndef _LOADER_H_
 #define _LOADER_H_
 
-#include "../common/types.h"
+/* This are the real functions that are used for our purpose */
+extern void LiCheckAndHandleInterrupts(void);
+extern int Loader_SysCallGetProcessIndex(int procId);
+extern int LiLoadAsync(const char *filename, unsigned int address, unsigned int address_size, int fileOffset, int fileType, int processIndex, int minus1);
+extern int LiWaitIopComplete(int unknown_syscall_arg_r3, int * remaining_bytes);
+extern int LiWaitIopCompleteWithInterrupts(int unknown_syscall_arg_r3, int * remaining_bytes);
 
-/* Instructions addresses in loader to replace by a "bl"(jump) to our replacement function */
-extern void addr_LOADER_Start(void);
-extern void addr_LOADER_Entry(void);
-extern void addr_LOADER_Prep(void);
-extern void addr_LiLoadRPLBasics_bef_LiWaitOneChunk(void);
-extern void addr_LiLoadRPLBasics_in_1_load(void);
-extern void addr_LiSetupOneRPL_after(void);
-extern void addr_GetNextBounce_bef_LiWaitOneChunk(void);
-extern void addr_LiRefillBounceBufferForReading_af_getbounce(void);
+extern int strncpy(char *dst, const char *src, int max_len);
+extern int strnlen(const char *src, int max_len);
+
+/* This are just addresses to the real functions which we only need as reference */
+extern int addr_LiBounceOneChunk(const char * filename, int fileType, int procId, int * hunkBytes, int fileOffset, int bufferNumber, int * dst_address);
+extern int addr_LiWaitOneChunk(unsigned int * iRemainingBytes, const char *filename, int fileType);
 
 #endif /* _LOADER_H_ */
